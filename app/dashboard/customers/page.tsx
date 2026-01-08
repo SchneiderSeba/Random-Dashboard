8ad4387446e0e4e1a31e25218121ec7898ceb44d
SchneiderSeba <seba_19_sc@hotmail.com>
2026-01-08 15:50:26 -0300

    ....

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -2,7 +2,14 @@ import { fetchCustomers } from "@/app/lib/data";
 import CustomersTable from "@/app/ui/customers/table";
 
 export default async function MainCustomers() {
-    const customers = await fetchCustomers();
+    const customersRaw = await fetchCustomers();
+    // Map to FormattedCustomersTable[]
+    const customers = customersRaw.map((customer: any) => ({
+        ...customer,
+        total_invoices: customer.total_invoices ?? 0,
+        total_pending: customer.total_pending ?? 0,
+        total_paid: customer.total_paid ?? 0,
+    }));
     return(
         <div>
             <CustomersTable customers={customers} />
